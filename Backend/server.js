9bd36da60fe7a73235277953319e570256132f46
fatimaamrch <fatima.amrouche@supdevinci-edu.fr>
2024-12-06 13:33:39 +0100

    redirection the player to the game

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -7,17 +6,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-
-let games = {}; 
+let games = {};  
 
 
 app.use(express.static('public'));
 
 
 app.post('/create-game', (req, res) => {
-    
     const gameId = Math.random().toString(36).substring(7); 
-    const secretNumber = Math.floor(Math.random() * 900000) + 100000;
+    const secretNumber = Math.floor(Math.random() * 900000) + 100000; 
 
     
     games[gameId] = {
@@ -34,6 +31,9 @@ app.post('/create-game', (req, res) => {
         gameId,
         message: `Partie créée avec succès ! Partagez ce lien pour inviter d'autres joueurs : ${gameLink}`
     });
+
+    
+    io.emit('game-info', { message: `La partie avec l'ID ${gameId} a été créée. Lien pour rejoindre : ${gameLink}` });
 });
 
 
@@ -42,12 +42,87 @@ app.get('/join-game/:gameId', (req, res) => {
 
     
     if (games[gameId]) {
-        res.sendFile(__dirname + '/public/index.html');  
+        res.sendFile(__dirname + '/public/game.html');  
     } else {
         res.status(404).send('Partie non trouvée');
     }
 });
 
+
+io.on('connection', (socket) => {
+    console.log('Un joueur est connecté');
+
+  
+    socket.on('join-game', (gameId) => {
+        if (games[gameId]) {
+            games[gameId].players.push(socket.id);  
+            socket.join(gameId);  
+            socket.emit('game-info', { message: `Vous avez rejoint la partie ${gameId}.` });
+            console.log(`Un joueur a rejoint la partie ${gameId}`);
+        } else {
+            socket.emit('game-info', { message: 'Partie non trouvée.' });
+        }
+    });
+
+   
+    socket.on('guess', (data) => {
+        const { gameId, guess } = data;
+        const game = games[gameId];
+
+        if (game) {
+            const guessNumber = parseInt(guess); 
+            let message = '';
+            let success = false;
+
+            
+            if (isNaN(guessNumber)) {
+                message = "Veuillez entrer un nombre valide.";
+            } else {
+                
+                if (guessNumber === game.secretNumber) {
+                    message = 'Félicitations, vous avez trouvé le bon nombre !';
+                    success = true;
+                } else {
+                    
+                    if (guessNumber > game.secretNumber) {
+                        message = 'Le nombre mystère est plus grand. Essayez encore !';
+                    } else if (guessNumber < game.secretNumber) {
+                        message = 'Le nombre mystère est plus petit. Essayez encore !';
+                    }
+
+                   
+                    const guessStr = guessNumber.toString();
+                    const secretStr = game.secretNumber.toString();
+                    
+                    let correctDigitsCount = 0;
+                    let secretNumberCopy = secretStr.split(''); 
+
+                    
+                    for (let i = 0; i < 6; i++) {
+                        const guessDigit = guessStr[i];
+                        const index = secretNumberCopy.indexOf(guessDigit); 
+
+                        if (index !== -1) {
+                            correctDigitsCount++; 
+                            secretNumberCopy.splice(index, 1); 
+                        }
+                    }
+
+                    message += ` Vous avez trouvé ${correctDigitsCount} chiffre(s) correct(s) parmi les 6.`;
+                }
+            }
+
+            
+            io.to(gameId).emit('result', { success, message });
+        } else {
+            socket.emit('game-info', { message: 'Partie non trouvée.' });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un joueur s\'est déconnecté');
+    });
+});
 
 
 server.listen(3000, () => {
